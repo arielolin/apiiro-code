3f17347041b7babb721b8999bd30fb8ae124762e
ariel.olin@apiiro.com <114854641+arielolin@users.noreply.github.com>
2024-10-21 11:39:19 +0300

    fix

--- src/modules/remediate-risks/suggestion-helper.ts
+++ src/modules/remediate-risks/suggestion-helper.ts
@@ -10,26 +10,12 @@ export async function addSuggestionLine(
   const document = editor.document;
   const position = new vscode.Position(lineNumber, 0);
 
-  const decorationType = vscode.window.createTextEditorDecorationType({
-    after: {
-      contentText: "  [Remediate]",
-      color: "green",
-      fontWeight: "bold",
-      border: "1px solid green",
-      margin: "0 0 0 10px",
-    },
-  });
-
-  const suggestionLine = `${originalText} // Suggestion: ${fixedText}`;
+  const suggestionLine = fixedText;
 
   const edit = new vscode.WorkspaceEdit();
   edit.insert(document.uri, position, suggestionLine + "\n");
 
   await vscode.workspace.applyEdit(edit);
-
-  editor.setDecorations(decorationType, [
-    new vscode.Range(lineNumber, 0, lineNumber, suggestionLine.length),
-  ]);
 
   const disposable = vscode.languages.registerCodeLensProvider(
     {
@@ -38,23 +24,46 @@ export async function addSuggestionLine(
     },
     {
       provideCodeLenses(document: vscode.TextDocument): vscode.CodeLens[] {
-        const range = new vscode.Range(
+        const originalRange = new vscode.Range(
+          lineNumber - 1,
+          0,
+          lineNumber - 1,
+          originalText.length,
+        );
+        const suggestionRange = new vscode.Range(
           lineNumber,
           0,
           lineNumber,
           suggestionLine.length,
         );
+
         return [
-          new vscode.CodeLens(range, {
-            title: "Remediate",
-            command: "extension.remediateRisk",
+          new vscode.CodeLens(originalRange, {
+            title: "Remediation Suggestion",
+            command: "extension.showRemediationMenu",
             arguments: [
               remediateAction,
-              decorationType,
               disposable,
               lineNumber,
               fixedText,
+              originalText,
             ],
+          }),
+          new vscode.CodeLens(suggestionRange, {
+            title: "Accept",
+            command: "extension.remediateRisk",
+            arguments: [
+              remediateAction,
+              disposable,
+              lineNumber,
+              fixedText,
+              originalText,
+            ],
+          }),
+          new vscode.CodeLens(suggestionRange, {
+            title: "Ignore",
+            command: "extension.ignoreRemediation",
+            arguments: [disposable, lineNumber],
           }),
         ];
       },
@@ -62,25 +71,88 @@ export async function addSuggestionLine(
   );
 
   vscode.commands.registerCommand(
-    "extension.remediateRisk",
+    "extension.showRemediationMenu",
     async (
       action: () => Promise<void>,
-      decType: vscode.TextEditorDecorationType,
       disp: vscode.Disposable,
       lineNum: number,
       fixedLine: string,
+      originalLine: string,
     ) => {
-      await action();
-      editor.edit((editBuilder) => {
-        editBuilder.replace(
-          new vscode.Range(lineNum - 1, 0, lineNum - 1, originalText.length),
+      const choice = await vscode.window.showQuickPick(
+        ["Apply Remediation", "Ignore"],
+        { placeHolder: "Choose an action" },
+      );
+
+      if (choice === "Apply Remediation") {
+        await remediateRisk(
+          editor,
+          action,
+          disp,
+          lineNum,
           fixedLine,
+          originalLine,
         );
-        editBuilder.delete(new vscode.Range(lineNum, 0, lineNum + 1, 0));
-      });
-      editor.setDecorations(decType, []);
-      decType.dispose();
-      disp.dispose();
+      } else if (choice === "Ignore") {
+        await ignoreRemediation(editor, disp, lineNum);
+      }
     },
   );
+
+  vscode.commands.registerCommand(
+    "extension.remediateRisk",
+    async (
+      action: () => Promise<void>,
+      disp: vscode.Disposable,
+      lineNum: number,
+      fixedLine: string,
+      originalLine: string,
+    ) => {
+      await remediateRisk(
+        editor,
+        action,
+        disp,
+        lineNum,
+        fixedLine,
+        originalLine,
+      );
+    },
+  );
+
+  vscode.commands.registerCommand(
+    "extension.ignoreRemediation",
+    async (disp: vscode.Disposable, lineNum: number) => {
+      await ignoreRemediation(editor, disp, lineNum);
+    },
+  );
+}
+
+async function remediateRisk(
+  editor: vscode.TextEditor,
+  action: () => Promise<void>,
+  disp: vscode.Disposable,
+  lineNum: number,
+  fixedLine: string,
+  originalLine: string,
+): Promise<void> {
+  await action();
+  await editor.edit((editBuilder) => {
+    editBuilder.replace(
+      new vscode.Range(lineNum - 1, 0, lineNum - 1, originalLine.length),
+      fixedLine,
+    );
+    editBuilder.delete(new vscode.Range(lineNum, 0, lineNum + 1, 0));
+  });
+  disp.dispose();
+}
+
+async function ignoreRemediation(
+  editor: vscode.TextEditor,
+  disp: vscode.Disposable,
+  lineNum: number,
+): Promise<void> {
+  await editor.edit((editBuilder) => {
+    editBuilder.delete(new vscode.Range(lineNum, 0, lineNum + 1, 0));
+  });
+  disp.dispose();
 }
